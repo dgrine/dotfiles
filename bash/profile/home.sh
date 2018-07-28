2438831eee79bb0525d7130a1d154a98b2605cf2
Djamel Grine <djamel.grine@gmail.com>
2018-07-28 22:07:01 +0200

    Added cdargo alias.

--- bash/profile/home.sh
+++ bash/profile/home.sh
@@ -1,3 +1,4 @@
+alias senvpd='tmp=`pwd` && cdpd && senv && cd $tmp'
 alias ssh_osmc='ssh osmc@192.168.1.2'
 alias cdpd='cdrepos; cd py-dev'
-alias senvpd='tmp=`pwd` && cdpd && senv && cd $tmp'
+alias cdargo='cdrepos; cd argo'
