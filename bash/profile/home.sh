ca805e3feccd95af81404c78997fda209fafc3f3
Djamel Grine <djamel.grine@gmail.com>
2018-08-03 17:31:38 +0200

    Clean-up.

--- bash/profile/home.sh
+++ bash/profile/home.sh
@@ -3,4 +3,4 @@ source ~/.my/setup/bash/profile/quadcore.sh
 alias senvpd='tmp=`pwd` && cdpd && senv && cd $tmp'
 alias ssh_osmc='ssh osmc@192.168.1.2'
 alias cdpd='cdrepos; cd py-dev'
-alias cdargo='cdrepos; cd argo'
+alias cdargo='cdrepos; cd Argo'
