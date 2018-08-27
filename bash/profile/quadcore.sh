8edae003a8d2fe794fc3c7a5634af6532b9bc9b2
Djamel Grine <djamel.grine@gmail.com>
2018-08-28 01:44:37 +0200

    Updated QuadCore profile.

--- bash/profile/quadcore.sh
+++ bash/profile/quadcore.sh
@@ -1,5 +1,4 @@
 alias ssh_qc='ssh grine@QuadCore -p 23'
 alias mount_qc='mkdir -p /Volumes/QuadCore && sshfs -p 23 grine@QuadCore:/ /Volumes/QuadCore -ovolname=quadcore'
-
 alias ssh_ez='ssh sgrine@ezpzugstlinux.ezpada.local'
 
