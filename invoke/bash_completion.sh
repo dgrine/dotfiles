2eb005385b81f59d1e538bd57a315d8b92df9fff
Djamel Grine <djamel@eyeontext.com>
2018-10-18 17:15:27 +0200

    Upgraded invoke completion.

--- invoke/bash_completion.sh
+++ invoke/bash_completion.sh
@@ -1,17 +1,30 @@
+# Invoke tab-completion script to be sourced with Bash shell.
+# Known to work on Bash 3.x, untested on 4.x.
 
-_invoke_list() {
-  COMPREPLY=()
-  local word="${COMP_WORDS[COMP_CWORD]}"
+_complete_invoke() {
+    local candidates
 
-  if [ "$COMP_CWORD" -eq 1 ]; then
-    COMPREPLY=( $(compgen -W "$(invoke --complete)" -- "$word") )
-  else
-    local words=("${COMP_WORDS[@]}")
-    unset words[0]
-    unset words[$COMP_CWORD]
-    local completions=$(invoke --complete "${words[@]}")
-    COMPREPLY=( $(compgen -W "$completions" -- "$word") )
-  fi
+    # COMP_WORDS contains the entire command string up til now (including
+    # program name).
+    # We hand it to Invoke so it can figure out the current context: spit back
+    # core options, task names, the current task's options, or some combo.
+    candidates=`invoke --complete -- ${COMP_WORDS[*]}`
+
+    # `compgen -W` takes list of valid options & a partial word & spits back
+    # possible matches. Necessary for any partial word completions (vs
+    # completions performed when no partial words are present).
+    #
+    # $2 is the current word or token being tabbed on, either empty string or a
+    # partial word, and thus wants to be compgen'd to arrive at some subset of
+    # our candidate list which actually matches.
+    #
+    # COMPREPLY is the list of valid completions handed back to `complete`.
+    COMPREPLY=( $(compgen -W "${candidates}" -- $2) )
 }
 
-complete -F _invoke_list inv invoke
+
+# Tell shell builtin to use the above for completing our invocations.
+# * -F: use given function name to generate completions.
+# * -o default: when function generates no results, use filenames.
+# * positional args: program names to complete for.
+complete -F _complete_invoke -o default invoke inv
