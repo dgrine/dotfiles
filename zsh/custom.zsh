7834e6e293cba059c8db767e46c16209249a066a
Djamel Grine <djamel@auro-technologies.com>
2018-11-19 16:11:56 +0100

    Switched to Z shell.

---
+++ zsh/custom.zsh
@@ -0,0 +1,164 @@
+# Assuming ~/.zshrc only sets export ZSH="/path/to/oh-my-zsh"
+# Sources this file.
+# Everything else in that file should be commented out
+
+ZSH_THEME="robbyrussell"
+
+plugins=(
+  git,
+  rake-fast
+)
+
+source $ZSH/oh-my-zsh.sh
+
+# Determing OS
+if [ "${OSTYPE}" = "darwin17.0" ]
+then
+    PLATFORM="mac"
+elif [ "${OSTYPE}" = "darwin18.0" ]
+then
+	PLATFORM="mac"
+elif [ "${OSTYPE}" = "linux-gnu" ]
+then
+	PLATFORM="linux"
+else
+	echo "Error: unsupported platform ${OSTYPE}"
+    return
+fi
+
+# Editor
+export EDITOR="vim -v"
+alias ce='$EDITOR'
+if [ "${PLATFORM}" = "mac" ]; then
+    if [ -x "$(command -v mvim)" ]; then
+        export VISUAL="mvim"
+    else
+        export VISUAL="${EDITOR}"
+        echo "Warning: MacVim not installed"
+    fi
+else
+    if [ -x "$(command -v gvim)" ]; then
+        export VISUAL="gvim"
+    else
+        export VISUAL="${EDITOR}"
+        echo "Warning: gvim not installed"
+    fi
+fi
+alias e='${VISUAL}'
+
+# Environment
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+if [ -x "$(command -v mysql)" ]; then
+    export PATH=$PATH:/usr/local/mysql/bin
+fi
+export PATH="/usr/local/sbin:$PATH"
+export PATH="$PATH:/$HOME/dev/bin"
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+#export DISPLAY=:0
+export XAUTHORITY=~/.Xauthority
+
+# Aliases
+alias cdvim='cd $HOME/.vim/'
+alias sshx='ssh -X -C -c blowfish-cbc,arcfour'
+alias evrc='e $HOME/.vimrc'
+alias ezrc='e $HOME/dev/repos/setup/zsh/custom.zsh'
+alias ezrcl='e $HOME/.zshrc_local'
+alias szrc='source $HOME/.zshrc'
+alias l='ls -alh'
+alias cddev='cd $HOME/dev'
+alias cdrepos='cd $HOME/dev/repos'
+alias cdsetup='cdrepos; cd setup'
+alias cddocs='cdrepos; cd docs'
+alias cdbin='cd $HOME/dev/bin'
+alias cdtmp='mkdir -p $HOME/dev/tmp && cd $HOME/dev/tmp';
+alias rake='noglob rake'
+if [ -x "$(command -v ranger)" ]; then
+    alias r='ranger --choosedir=$HOME/.rangerdir; LASTDIR=`cat $HOME/.rangerdir`; cd "$LASTDIR"'
+else
+    echo "Warning: ranger not installed"
+fi
+alias scpalt='rsync avzP'
+alias grep='grep -E -n --color=auto'
+if [ -x "$(command -v python3)" ]; then
+    alias python='python3'
+    alias pip='pip3'
+    alias mkenv='python3 -m venv env'
+    alias senv='source env/bin/activate'
+else
+    echo "Warning: Python 3 not installed"
+fi
+if [ -x "$(command -v make)" ]; then
+    alias m='make -j7'
+else
+    echo "Warning: Make not installed"
+fi
+if [ -x "$(command -v pygmentize)" ]; then
+    alias pcat='pygmentize -O style=native -g'
+else
+    echo "Warning: Pygmentize not installed"
+fi
+if [ "${PLATFORM}" = "mac" ]; then
+    alias meld='open /Applications/Meld.app'
+fi
+if [ -x "$(command -v lldb)" ]; then
+    function dbg {
+        prog=$1
+        shift
+        if [ ! -f dbg.txt ]; then
+            lldb $prog -- $@
+        else
+            echo "Using dbg.txt"
+            lldb -S dbg.txt $prog -- $@
+        fi
+    }
+fi
+if [ -x "$(command -v git)" ]; then
+    alias s='git status'
+    alias b='echo Top-level; git branch; git submodule foreach git branch'
+    alias d='git diff'
+    alias a='git add .'
+    alias cm='git commit -m '
+    alias cuth='git commit -m uth'
+    alias cwip='cm wip'
+    alias pr='git pull --rebase'
+    alias p='git push'
+    alias ms='git merge --squash'
+    alias ch='git checkout'
+    alias master='ch master'
+    alias gc='git clone -j8 --recursive'
+    alias gd='git difftool'
+    alias gdd='git difftool'
+else
+    echo "Warning: Git not installed"
+fi
+
+# iTerm shell integration
+#test -e "${HOME}/.iterm2_shell_integration.bash" && source "${HOME}/.iterm2_shell_integration.bash"
+
+#Fzf
+if [ -x "$(command -v fzf)" ]; then
+    export FZF_CTRL_T_OPTS='--height 90% --preview "pygmentize -l $(pygmentize -N {}) {}"'
+    [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+else
+    echo "Warning: fzf not installed"
+fi
+
+# Bash completion
+#if [ "${PLATFORM}" = "mac" ]; then
+	#TMP_VAR=`find /usr/local/Cellar/bash-completion/ -name bash_completion | tail -n 1`
+	#if [ "${TMP_VAR}" != "" ]; then
+		#. ${TMP_VAR}
+	#fi
+#fi
+
+# Invoke completion
+#if [ -x "$(command -v inv)" ]; then
+    #source ${HOME}/dev/repos/setup/invoke/bash_completion.sh
+#else
+    #echo "Warning: Invoke not installed"
+#fi
+
+# Local profile
+[ -f "${HOME}/.zshrc_local" ] && source ${HOME}/.zshrc_local
---
+++ zsh/profile/auro.zsh
@@ -0,0 +1,89 @@
+alias cdcd3='cdrepos; cd auro-codec-v3'
+alias cdcd4='cdrepos; cd auro-codec-v4'
+alias cdcx1='cdrepos; cd auro-cx-v1'
+alias cdcx2='cdrepos; cd auro-cx-v2'
+alias cdavs='cdrepos; cd fusion-avs'
+alias cdtop='cdrepos; cd toplevel-fusion'
+alias cdcook='cdrepos; cd cook-binary'
+alias cd548='cdavs; cd story/code548'
+
+function auro_init {
+    auro_compiler=""
+    if [ "${auro_brand}" != "" ]
+    then
+        auro_compiler=$auro_brand
+    else
+        echo "Error: Auro brand not set (e.g. clang, or gcc, ...)"
+    fi
+    if [ "${auro_arch}" != "" ]
+    then
+        auro_compiler=$auro_compiler-$auro_arch
+    else
+        echo "Error: Auro architecture not set (e.g. x64)"
+    fi
+    if [ "${auro_mode}" != "" ]
+    then
+        auro_compiler=$auro_compiler-$auro_mode
+    else
+        echo "Error: Auro mode not set (e.g. debug, release)"
+    fi
+    if [ "${auro_rtc}" != "" ]
+    then
+        auro_compiler=$auro_compiler-$auro_rtc
+    fi
+    if [ "${auro_postfix}" != "" ]
+    then
+        auro_compiler=$auro_compiler-$auro_postfix
+    fi
+    export auro_compiler=$auro_compiler
+}
+
+function debug {
+    export auro_mode="debug"
+    auro_init
+    auro
+}
+
+function release {
+    export auro_mode="release"
+    auro_init
+    auro
+}
+
+function noverbose {
+    unset auro_verbose
+}
+
+function verbose {
+    export auro_verbose=1
+}
+
+function nortc {
+    export auro_rtc=""
+    auro_init
+    auro
+}
+
+function rtc {
+    export auro_rtc="rtc"
+    auro_init
+    auro
+}
+
+function auro {
+    echo $auro_compiler
+}
+
+function default {
+    if [ "${OSTYPE}" = "linux-gnu" ]; then
+        export auro_brand="gcc"
+    else
+        export auro_brand="clang"
+    fi
+    export auro_arch="x64"
+    debug
+    rtc
+    noverbose
+}
+
+default > /dev/null
