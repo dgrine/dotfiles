d711709bac433c75fe6c23efe12001a2d290b63f
Djamel Grine <djamel.grine@gmail.com>
2018-12-15 11:12:57 +0100

    Updated home profile.

--- zsh/profile/home.zsh
+++ zsh/profile/home.zsh
@@ -1,5 +1,5 @@
-source ~/dev/repos/setup/bash/profile/humanhuman.sh
-source ~/dev/repos/setup/bash/profile/quadcore.sh
+source ~/dev/repos/setup/zsh/profile/humanhuman.zsh
+source ~/dev/repos/setup/zsh/profile/quadcore.zsh
 alias senvpd='tmp=`pwd` && cdpd && senv && cd $tmp'
 alias ssh_osmc='ssh osmc@192.168.1.2'
 alias cdpd='cdrepos; cd py-dev'
