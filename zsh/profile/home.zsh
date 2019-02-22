5cbd8942d0df2299f26f87dab5325bec9e1f6735
Djamel Grine <djamel.grine@gmail.com>
2019-02-22 11:36:14 +0100

    RDP port fowarding.

--- zsh/profile/home.zsh
+++ zsh/profile/home.zsh
@@ -3,6 +3,7 @@ source ~/dev/repos/setup/zsh/profile/quadcore.zsh
 alias senvpd='tmp=`pwd` && cdpd && senv && cd $tmp'
 alias ssh_osmc='ssh osmc@192.168.1.2'
 alias ssh_deeplearning='ssh -p 24 grine@deeplearning -L 8080:localhost:8080'
+alias ssh_deeplearning_rdp='ssh -p 24 grine@deeplearning -L 3389:localhost:3389 -N'
 alias cdpd='cdrepos; cd py-dev'
 alias cdargo='cdrepos; cd Argo'
 alias cddrl='cdrepos; cd drl'
