2c39f868057e15a7c8a1a05a7caddf68ee83f0bf
Djamel Grine <djamel.grine@gmail.com>
2019-03-01 21:07:42 +0100

    Renamed deeplearning to blackbox.

--- zsh/profile/home.zsh
+++ zsh/profile/home.zsh
@@ -2,8 +2,8 @@ source ~/dev/repos/setup/zsh/profile/humanhuman.zsh
 source ~/dev/repos/setup/zsh/profile/quadcore.zsh
 alias senvpd='tmp=`pwd` && cdpd && senv && cd $tmp'
 alias ssh_osmc='ssh osmc@192.168.1.2'
-alias ssh_deeplearning='ssh -p 24 grine@deeplearning -L 8080:localhost:8080'
-alias ssh_deeplearning_rdp='ssh -p 24 grine@deeplearning -L 3389:localhost:3389 -N'
+alias ssh_blackbox='ssh -p 24 grine@blackbox -L 8080:localhost:8080'
+alias ssh_blackbox_rdp='ssh -p 24 grine@blackbox -L 3389:localhost:3389 -N'
 alias cdpd='cdrepos; cd py-dev'
 alias cdargo='cdrepos; cd Argo'
 alias cddrl='cdrepos; cd drl'
