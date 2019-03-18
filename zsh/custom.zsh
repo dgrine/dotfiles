e9fe9345eaf5a3c65bd03fb8f97db6c49e5fc5bd
Djamel Grine <djamel@auro-technologies.com>
2019-03-18 13:02:04 +0100

    Miniconda.

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -97,6 +97,21 @@ else
     echo "Warning: Python 3 not installed"
 fi
 
+# Miniconda
+__conda_setup="$(CONDA_REPORT_ERRORS=false '$HOME/miniconda3/bin/conda' shell.bash hook 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    \eval "$__conda_setup"
+else
+    if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniconda3/etc/profile.d/conda.sh"
+        CONDA_CHANGEPS1=false conda activate base
+    else
+        \export PATH="$HOME/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda init <<<
+
 # Make
 if [ -x "$(command -v make)" ]; then
     alias m='make -j7'
