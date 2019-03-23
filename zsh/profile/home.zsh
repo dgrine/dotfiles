92708ed3893877085a4430392ca88b4f8557dafb
Djamel Grine <djamel.grine@gmail.com>
2019-03-23 21:46:07 +0100

    Port forwarding.

--- zsh/profile/home.zsh
+++ zsh/profile/home.zsh
@@ -2,8 +2,13 @@ source ~/dev/repos/setup/zsh/profile/humanhuman.zsh
 source ~/dev/repos/setup/zsh/profile/quadcore.zsh
 alias senvpd='tmp=`pwd` && cdpd && senv && cd $tmp'
 alias ssh_osmc='ssh osmc@192.168.1.2'
-alias ssh_blackbox='ssh -p 24 grine@blackbox -L 8080:localhost:8080'
-alias ssh_blackbox_rdp='ssh -p 24 grine@blackbox -L 3389:localhost:3389 -N'
+alias ssh_deeplearning='ssh -p 24 grine@deeplearning'
+function ssh_deeplearning_fwd()
+{
+    port="$1"
+    echo "Forwarding port $port ..."
+    ssh_deeplearning -L "$port":localhost:"$port" -N
+}
 alias cdpd='cdrepos; cd py-dev'
 alias cdargo='cdrepos; cd Argo'
 alias cddrl='cdrepos; cd drl'
