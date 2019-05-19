e7601b7dac1515c25e63743aa8f4324be1f64c1e
Djamel Grine <djamel.grine@gmail.com>
2019-05-19 10:36:56 +0200

    Migrated quadcore profile to home profile and added mounting to deeplearning.

--- zsh/profile/home.zsh
+++ zsh/profile/home.zsh
@@ -1,8 +1,17 @@
 source ~/dev/repos/setup/zsh/profile/humanhuman.zsh
-source ~/dev/repos/setup/zsh/profile/quadcore.zsh
 alias senvpd='tmp=`pwd` && cdpd && senv && cd $tmp'
 alias ssh_osmc='ssh osmc@192.168.1.2'
+
+# Deeplearning
 alias ssh_deeplearning='ssh -p 24 grine@deeplearning'
+alias mount_deeplearning='mkdir -p ~/dev/mnt/deeplearning && sshfs -p 24 grine@deeplearning:/ ~/dev/mnt/deeplearning -ovolname=deeplearning,auto_cache,reconnect,defer_permissions,no_readahead,noappledouble,negative_vncache'
+
+# Quadcore
+alias ssh_qc='ssh grine@QuadCore -p 23'
+alias mount_qc='mkdir -p ~/dev/mnt/QuadCore && sshfs -p 23 grine@QuadCore:/ ~/dev/mnt/QuadCore -ovolname=quadcore'
+
+# Ezpada
+alias ssh_ez='ssh sgrine@ezpzugstlinux.ezpada.local'
 function ssh_deeplearning_fwd()
 {
     port="$1"
