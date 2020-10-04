007207538e5a68dce4c95a9f54a2a14f530b4ed8
Djamel Grine <djamel.grine@gmail.com>
2020-10-04 21:20:07 +0200

    IPython wip

--- ipython/profile_auro/startup/00_bootstrap.py
+++ ipython/profile_auro/startup/00_bootstrap.py
@@ -1,3 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.ion()
 from pathlib import Path
 import os
 import sys
@@ -8,4 +12,3 @@ import auro.build.bootstrap
 print("Tip: For automatic reloading and interactive plots, use:")
 print("%load_ext autoreload")
 print("%autoreload 2")
-print("plt.ion()")
