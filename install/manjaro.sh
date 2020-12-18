9b5b94d2141fa653e295fc83e0ef405ca0d73995
Djamel Grine <djamel.grine@gmail.com>
2020-12-18 23:18:29 +0100

    manjaro

--- install/manjaro.sh
+++ install/manjaro.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
-sudo pamac install alacritty
+pamac install alacritty
 mkdir -p $HOME/dev/repos
 cd $HOME/dev/repos
 git clone git@gitlab.com:dgrine/setup.git
 
 # neovim
-sudo pamac install neovim
+pamac install neovim
 cd $HOME/.config
 ln -s $HOME/dev/repos/setup/alacritty
 
 # neovim: coc requires node
-sudo pamac install nodejs
+pamac install nodejs
 
 # tmux
-sudo pamac install tmux
+pamac install tmux
 cd $HOME
 ln -s  $HOME/dev/repos/setup/tmux/.tmux.conf
 
+# fzf
+pamac install fzf
+cd $HOME
+ln -s $HOME/dev/repos/setup/fzf/.fzf.zsh
+
+# ripgrep
+pamac install ripgrep
+
 # alacrity
-sudo pamac install alacritty
+pamac install alacritty
 cd $HOME/.config
 ln -s $HOME/dev/repos/setup/alacritty
 
 # vifm
-sudo pamac install vifm
+pamac install vifm
 cd $HOME/.config
 ln -s $HOME/dev/repos/setup/vifm
 
