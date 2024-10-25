dc7ea4214b1079eac20f21d0cb215798056a57b1
Djamel Grine <djamel@blackboardbv.be>
2024-10-25 12:01:02 -0300

    npm

--- home-manager/programs/nodejs/nodejs.nix
+++ home-manager/programs/nodejs/nodejs.nix
@@ -1,9 +1,17 @@
 { pkgs, ... }:
 
 {
-    home.packages = with pkgs; [
-        nodejs
-        yarn
-    ];
-    programs.zsh.oh-my-zsh.plugins = ["npm"];
+    home = {
+        file.".npmrc".source = ../../../npm/.npmrc;
+        packages = with pkgs; [
+            nodejs
+            yarn
+        ];
+    };
+    programs.zsh = {
+        oh-my-zsh.plugins = ["npm"];
+        initExtra = ''
+            export PATH=$PATH:~/.npm-packages/bin
+        '';
+    };
 }
