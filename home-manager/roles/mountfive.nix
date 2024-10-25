c01731b831542cab076a157fdc6f485d2af36590
Djamel Grine <djamel@blackboardbv.be>
2024-10-25 12:01:58 -0300

    role: mountfive

---
+++ home-manager/roles/mountfive.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+let M5_NAME = "mountfive"; in {
+    programs.zsh = let M5_ROOT = "$HOME/dev/roles/${M5_NAME}"; in {
+        initExtra = ''
+            # Add the root directory
+            mkdir -p ${M5_ROOT}
+
+            # Add the repos directory
+            mkdir -p ${M5_ROOT}/repos
+
+            # Add the docs directory
+            mkdir -p ${M5_ROOT}/docs
+
+            # Add a tmux session
+            tmux new-session -d -s ${M5_NAME} &> /dev/null
+        '';
+        shellAliases = {
+
+        };
+    };
+}
+
