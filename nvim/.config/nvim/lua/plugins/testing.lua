76808d76805c6e55a9017b179b1ffd998c1453b0
Djamel Grine <djamelg@macmini.local>
2025-07-12 15:50:41 +0200

    Format testing

--- nvim/.config/nvim/lua/plugins/testing.lua
+++ nvim/.config/nvim/lua/plugins/testing.lua
@@ -1,28 +1,41 @@
 return {
-    {
-        "nvim-neotest/neotest",
-        dependencies = {
-            "antoinemadec/FixCursorHold.nvim",
-            "nvim-lua/plenary.nvim",
-            "nvim-neotest/neotest-python",
-            "nvim-neotest/nvim-nio",
-            "nvim-treesitter/nvim-treesitter",
-        },
-        config = function()
-            local neotest = require("neotest")
-            neotest.setup({
-                adapters = {
-                    require("neotest-python")({
-                        dap = { justMyCode = true },
-                    }),
-                },
-            })
-            local wk = require("which-key")
-            wk.add( { "<Leader>t", group = "Test", icon = { icon = "", color = "grey" } } )
-            wk.add( { "<Leader>tt", neotest.run.run, desc = "Nearest", icon = { icon = "", color = "grey" } } )
-            wk.add( { "<Leader>td", function() neotest.run.run(vim.fn.expand("%")) end, desc = "Document", icon = { icon = "󰈙", color = "grey" } } )
-            wk.add( { "<Leader>to",neotest.output_panel.toggle, desc = "Output", icon = { icon = "", color = "grey" } } )
-            wk.add( { "<Leader>ts",neotest.summary.toggle, desc = "Summary", icon = { icon = "", color = "grey" } } )
-        end,
-    },
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-neotest/neotest-python",
+			"nvim-neotest/nvim-nio",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			local neotest = require("neotest")
+			neotest.setup({
+				adapters = {
+					require("neotest-python")({
+						dap = { justMyCode = true },
+						runer = "pytest",
+					}),
+				},
+			})
+			local wk = require("which-key")
+			wk.add({ "<Leader>t", group = "Test", icon = { icon = "", color = "grey" } })
+			wk.add({ "<Leader>tt", neotest.run.run, desc = "Nearest", icon = { icon = "", color = "grey" } })
+			wk.add({
+				"<Leader>td",
+				function()
+					neotest.run.run(vim.fn.expand("%"))
+				end,
+				desc = "Document",
+				icon = { icon = "󰈙", color = "grey" },
+			})
+			wk.add({
+				"<Leader>to",
+				neotest.output_panel.toggle,
+				desc = "Output",
+				icon = { icon = "", color = "grey" },
+			})
+			wk.add({ "<Leader>ts", neotest.summary.toggle, desc = "Summary", icon = { icon = "", color = "grey" } })
+		end,
+	},
 }
